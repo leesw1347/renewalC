250cf3637a854ec9808d2a0a2f02d0fcde393943
leesw1347 <leesw1347@gmail.com>
2022-08-18 13:14:25 +0900

    [ADD] 템플릿 특수화를 통하여, 템플릿별 별도의 타입으로 작업을 진행할 수 있음

---
+++ Template/Template-Speicalization/Template07.cpp
@@ -0,0 +1,17 @@
+//
+// Created by lsw94 on 22-08-18.
+//
+
+int main(int argc, char *argv[]) {
+
+    /*
+     * 템플릿 특수화에는 2가지 종류가 존재한다.
+     *  1) 전체 템플릿 특수화 (Explicit Full Template Specializtaion) :
+     *      템플릿에서 선언한 템플릿 매개변수 전체에 대해 특정 데이터 타입으로 템플릿 인수를 지정하는 경우를 말한다
+     *
+     *  2) 부분 템플릿 특수화 (Partial Template Specialization) :
+     *      템플릿에서 선언한 템플릿 매개변수 가운데 일부에 대해 특정 데이터 타입으로 인수를 지정하는 경우를 말한다
+     * */
+
+
+}
